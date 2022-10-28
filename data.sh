821ef61556fe935743f3bbeed69854aa6a7b20ea
RizalXploit <116887451+RizalXploit@users.noreply.github.com>
2022-10-28 18:54:38 +0700

    Add files via upload

---
+++ data.sh
@@ -0,0 +1,21 @@
+clear
+
+echo "selamat datang kontol"
+read -p "lanjut hack? y/n "
+
+
+
+
+read -p "data siapa? " nama
+
+
+echo "mengumpulkan informasi..."
+sleep "60"
+echo "Mendapatkan Alamat Rumah....."
+sleep "60"
+echo "mengumpulkan data....."
+echo "Hasil: "
+echo "Nama: $nama"
+echo "Tanggal Lahir: 17 agustus 1945"
+echo "Pekerjaan : tukang kayu"
+echo "Alamat Lengkap: km 190 tol cipali"
